8b02996af4c628d7741e04a7c84ab73ef9bbb7ef
erichongrac <148745337+erichongrac@users.noreply.github.com>
2025-08-02 22:19:07 +0800

    Used Column

--- backend/WhereToGo.Api/Models/Place.cs
+++ backend/WhereToGo.Api/Models/Place.cs
@@ -1,29 +1,41 @@
 ﻿#nullable enable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.WhereToGo.Api.Models
 {
     public class Place
     {
+        [Column("id")] // Tell EF to map to lowercase "id"
         public int Id { get; set; }
 
+        [Column("name")]
         public required string Name { get; set; }
 
+        [Column("city")]
         public required string City { get; set; }
 
+        [Column("country")]
         public required string Country { get; set; }
 
+        [Column("address")]
+
         public required string Address { get; set; }
+        [Column("description")]
 
         public string? Description { get; set; }
 
+        [Column("latitude")]
         public double Latitude { get; set; }
 
+        [Column("longitude")]
         public double Longitude { get; set; }
 
+        [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        [Column("updated_at")]
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         // Optional: to associate places with users (future feature)
